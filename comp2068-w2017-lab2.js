dfc168b9fac1de7a695774c30b55e1c0c269ec67
Nicholas Oshukany <ozzie347@hotmail.com>
2017-01-18 17:43:15 -0500

    Finished Lab

--- comp2068-w2017-lab2.js
+++ comp2068-w2017-lab2.js
@@ -1,4 +1,5 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
+console.log("Your pick: " + userChoice);
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
@@ -6,34 +7,35 @@ if (computerChoice < 0.34) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+} console.log("Computer's pick: " + computerChoice);
 
-var compare = function(userChoice, computerChoice){
-    if (userChoice === computerChoice){
+var compare = function(choice1, choice2){
+    if (choice1 === choice2){
         return "The result is a tie!";
-    }else if (userChoice === "rock"){
-        if (computerChoice === "scissors"){
-            return "rock wins";
+    }else if (choice1 === "rock"){
+        if (choice2 === "scissors"){
+            return "Rock wins";
         }
         else {
-            return "paper wins";
+            return "Paper wins";
         }
     }
-    else if (userChoice === "paper"){
-        if (computerChoice === "rock"){
-            return "paper wins";
+    else if (choice1 === "paper"){
+        if (choice2 === "rock"){
+            return "Paper wins";
         }
         else {
-            return "scissors wins";
+            return "Scissors wins";
         }
     }
-    else if (userChoice === "scissors"){
-        if (computerChoice === "paper"){
-            return "scissors wins";
+    else if (choice1 === "scissors"){
+        if (choice2 === "paper"){
+            return "Scissors wins";
         }
         else {
-            return "rock wins";
+            return "Rock wins";
         }
     }
-}
-compare(userChoice, computerChoice);
+};
+
+console.log(compare(userChoice, computerChoice));
