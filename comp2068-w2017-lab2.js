2d3826785354b87a5ab576e657a019a73e238c9b
Nicholas Oshukany <ozzie347@hotmail.com>
2017-01-18 14:32:00 -0500

    first commit, after class file

---
+++ comp2068-w2017-lab2.js
@@ -0,0 +1,10 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
